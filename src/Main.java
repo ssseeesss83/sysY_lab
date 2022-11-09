cc0f7b27c3d531117b184b8a188ce3214354c010
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-09 22:03:44 +0800

    > make 201250210 yuhaofeng Linux yuhaofeng 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:03:44 up 1 min,  1 user,  load average: 1.35, 0.48, 0.17 4a0b275d28e8da8cd476c32d336bcf52d92c1dc2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
 import java.io.*;
+import java.util.List;
+
 public class Main
-{    
+{
     public static void main(String[] args) throws IOException {
-        FileReader fr = new FileReader(args[0]);
-        BufferedReader br = new BufferedReader(fr);
-        String line;
-        while((line = br.readLine()) != null){
-            System.out.println(line);
+//        if (args.length < 1) {
+//            System.err.println("input path is required");
+//        }
+        //String source = args[0];
+        String source = "E:\\bianyiyuanli\\Lab\\src\\a.txt";
+        CharStream input = CharStreams.fromFileName(source);
+        SysYLexer sysYLexer = new SysYLexer(input);
+        sysYLexer.removeErrorListeners();
+        MyErrorListener myErrorListener = new MyErrorListener();
+        sysYLexer.addErrorListener(myErrorListener);
+        List<? extends Token> tokens = sysYLexer.getAllTokens();
+        String[] ruleNames = sysYLexer.getRuleNames();
+        if(!myErrorListener.hasError) {
+            for (Token token : tokens) {
+                System.err.println(ruleNames[token.getType() - 1] + " " + token.getText() + " " + "at" + " " + "Line" + " " + token.getLine() + ".");
+            }
         }
     }
 }
---
+++ src/MyErrorListener.java
@@ -0,0 +1,21 @@
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+
+import java.util.BitSet;
+
+public class MyErrorListener extends BaseErrorListener {
+    public Boolean hasError = false;
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        //super.syntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg, e);
+            if(msg.charAt(msg.length()-2) != ' ') {
+                System.err.println("Error type A at Line " + line + ":" + msg);
+                hasError = true;
+            }
+    }
+}
