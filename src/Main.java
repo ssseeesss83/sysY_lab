fe6e880116348c4addbc4ce27f861f99a179a747
Destroy <2087953253@qq.com>
2022-11-22 20:39:38 +0800

    10

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class Main
 {
     public static void main(String[] args) throws IOException {
-//        if (args.length < 1) {
-//            System.err.println("input path is required");
-//        }
-//        String source = args[0];
-        String source = "E:\\bianyiyuanli\\Lab\\src\\a.txt";
+        if (args.length < 1) {
+            System.err.println("input path is required");
+        }
+        String source = args[0];
+        //String source = "E:\\bianyiyuanli\\Lab\\src\\a.txt";
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
         //String[] ruleNames = sysYLexer.getRuleNames();
